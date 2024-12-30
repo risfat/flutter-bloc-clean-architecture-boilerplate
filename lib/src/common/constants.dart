5ecc2ec3dd2067ba1593bb931d89820ec5e1d401
Md Risfat <Risfat.bd@gmail.com>
2024-12-30 18:15:08 +0600

    [New] Added HTTP Client ( DIO ) & Auth Interceptor & Logging

--- lib/src/common/constants.dart
+++ lib/src/common/constants.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: constant_identifier_names
 
 const String ACCESS_TOKEN = 'access_token';
+const String REFRESH_TOKEN = 'refresh_token';
+const String TOKEN_EXPIRY = 'token_expiry';
 const String ONBOARDING = 'onboarding';
 
+const String USER_LAST_NAME = 'user_last_name';
+const String USER_EMAIL = 'user_email';
 
 const double MARGIN = 18;
 const double RADIUS = 8;
--- lib/src/common/failures.dart
+++ lib/src/common/failures.dart
@@ -1,7 +1,6 @@
 import 'package:equatable/equatable.dart';
 
 abstract class Failure extends Equatable {
-
   const Failure(this.message);
   final String message;
 
@@ -10,17 +9,39 @@ abstract class Failure extends Equatable {
 }
 
 class ServerFailure extends Failure {
-  const ServerFailure(String message) : super(message);
+  const ServerFailure(super.message);
 }
 
 class ConnectionFailure extends Failure {
-  const ConnectionFailure(String message) : super(message);
+  const ConnectionFailure(super.message);
 }
 
 class DatabaseFailure extends Failure {
-  const DatabaseFailure(String message) : super(message);
+  const DatabaseFailure(super.message);
 }
 
 class CacheFailure extends Failure {
-  const CacheFailure(String message) : super(message);
+  const CacheFailure(super.message);
+}
+
+class NoDataFailure extends Failure {
+  const NoDataFailure(super.message);
+}
+
+class UserNotFoundFailure extends Failure {
+  const UserNotFoundFailure(super.message);
+}
+
+class BadRequestFailure extends Failure {
+  const BadRequestFailure(super.message);
+}
+
+class UnexpectedFailure extends Failure {
+  const UnexpectedFailure(super.message);
+}
+
+class ValidationFailure extends Failure {
+  final List<String> errors;
+
+  ValidationFailure(this.errors) : super(errors.join(', '));
 }
